69dc1f92312de883ca11ed2de1b3aa78b9af14cb
saipradeepmorla <morlasaipradeep@gmail.com>
2024-12-26 09:37:06 +0000

    almost done

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      {/* https://youtu.be/zA9r5zTllx4?si=K2rMNZ7MDP_h5qOr&t=8541
+      // 2:22:20 */}
+    </div>
+  );
+};
+
+export default Contact;
